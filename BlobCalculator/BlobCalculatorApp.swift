60c93363a27c2e69225bf753acba2bf817f6f27c
Nathan Cho <ncho@esri.com>
2025-12-26 10:44:31 -0800

    Initial Commit

---
+++ BlobCalculator/BlobCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlobCalculatorApp.swift
+//  BlobCalculator
+//
+//  Created by Nathan Cho on 12/26/25.
+//
+
+import SwiftUI
+
+@main
+struct BlobCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BlobCalculatorTests/BlobCalculatorTests.swift
@@ -0,0 +1,17 @@
+//
+//  BlobCalculatorTests.swift
+//  BlobCalculatorTests
+//
+//  Created by Nathan Cho on 12/26/25.
+//
+
+import Testing
+@testable import BlobCalculator
+
+struct BlobCalculatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
